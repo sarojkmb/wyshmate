30dcd10f2e8db831a10e86839dd2384bb3594e57
Saroj <saroj@Sarojs-MacBook-Pro.local>
2026-03-26 23:14:37 -0400

    Remove legacy Java API

--- api/src/main/java/com/wyshmate/api/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wyshmate.api.repository;
-
-import com.wyshmate.api.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface BoardRepository extends JpaRepository<Board, UUID> {
-}
